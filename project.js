2ed83e3643393fc4df7739dfafec39fbeb5bb2a4
FAZ KAY <fkydln@gmail.com>
2021-08-09 01:07:33 -0700

    Collecting, Managing, Serving imgs are downloaded to local.

--- project.js
+++ project.js
@@ -1,2 +1,7 @@
 console.log("Starting off");
 //Writing the algorithm:
+//Donate Now Checkout:
+// document.getElementById("donateCheckout").addEventListener('click', function(){
+
+// } )
+var myModal = new bootstrap.Modal(document.getElementById("myModal"), options);
